1b289c2ad6bb238ecc4cf28a3e5bc8073c431c06
José Luis Quintana <joseluisquintana20@gmail.com>
2016-11-16 22:23:41 -0500

    Browser & AMD support

--- dist/quek.js
+++ dist/quek.js
@@ -1,22 +1,36 @@
 /*! quek v1.0.1 | MIT (c) 2016 José Luis Quintana */
-"use strict";
+'use strict';
 
-module.exports = function () {
-  var queue = [];
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-  return {
-    enqueue: function enqueue(value) {
-      queue.push(value);
-    },
-    dequeue: function dequeue() {
-      queue.shift();
-    },
-    peek: function peek() {
-      return queue[0];
-    },
-    length: function length() {
-      return queue.length;
-    }
+(function () {
+  var Quek = function Quek() {
+    var queue = [];
+
+    return {
+      enqueue: function enqueue(value) {
+        queue.push(value);
+      },
+      dequeue: function dequeue() {
+        queue.shift();
+      },
+      peek: function peek() {
+        return queue[0];
+      },
+      length: function length() {
+        return queue.length;
+      }
+    };
   };
-};
+
+  if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object' && _typeof(module.exports) === 'object') {
+    module.exports = Quek;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return Quek;
+    });
+  } else {
+    window.Quek = Quek;
+  }
+})();
 
--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-module.exports = () => {
-  const queue = []
+(() => {
+  const Quek = () => {
+    const queue = []
 
-  return {
-    enqueue: value => {
-      queue.push(value)
-    },
-    dequeue: () => {
-      queue.shift()
-    },
-    peek: () => {
-      return queue[0]
-    },
-    length: () => {
-      return queue.length
+    return {
+      enqueue: value => {
+        queue.push(value)
+      },
+      dequeue: () => {
+        queue.shift()
+      },
+      peek: () => {
+        return queue[0]
+      },
+      length: () => {
+        return queue.length
+      }
     }
   }
-}
+
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = Quek
+  } else if (typeof define === 'function' && define.amd) {
+    define([], () => {
+      return Quek
+    })
+  } else {
+    window.Quek = Quek
+  }
+})()
